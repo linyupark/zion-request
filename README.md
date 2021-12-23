46f6ac8c3d2ef2c2c9e5f192feea0b84e0ee3a4c
linyupark <linyupark@gmail.com>
2021-12-23 15:42:50 +0800

    feat(Add params SWR response and initialData parameter = swr.fallbackData, mutate => swrMutate, mutate will uppdate the local data immediately, but disable the revalidation)

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Use [SWR](https://swr.vercel.app/zh-CN) in the way of [useRequest](https://ahook
 
 ### changelog
 * [0.0.1] Add default parameters, manually execute functions, and refresh interfaces that match useRequest, etc.
+* [0.0.2] Add params SWR response and initialData parameter = swr.fallbackData, mutate => swrMutate, mutate will uppdate the local data immediately, but disable the revalidation
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import { useState } from 'react'
-import useSWR, { SWRConfiguration, SWRResponse } from 'swr'
+import useSWR, { MutatorCallback, SWRConfiguration, SWRResponse } from 'swr'
+
+interface NewData {
+  [key: string]: any
+}
 
 interface UseRequestOptions {
   // The request id defaults to''
   key?: string
   manual?: boolean
   // Default request parameters
-  params?: any
+  params: null | any
+  // Default data alias swr.fallbackData
+  initialData?: any
+  // SWR config
   swr?: SWRConfiguration
 }
 
-interface UseRequestResp extends SWRResponse {
+interface UseRequestResp extends Omit<SWRResponse, 'mutate'> {
   loading: boolean
   run: (newParams?: any) => void
   refresh: () => void
+  params?: any
+  mutate: (newData: NewData) => void
+  swrMutate: <Data = any>(
+    data?: Data | Promise<Data> | MutatorCallback<Data>,
+    shouldRevalidate?: boolean,
+  ) => Promise<Data | undefined>
 }
 
 /**
@@ -30,6 +43,7 @@ const useRequest = (
     key: '',
     manual: false, // Manual execution
     params: null,
+    initialData: undefined,
     ...options,
     swr: {
       // https://swr.vercel.app/zh-CN/docs/options
@@ -47,11 +61,10 @@ const useRequest = (
   const [params, setParams] = useState(mergeOptions.params)
 
   // Main
-  const swrResp: SWRResponse = useSWR(
-    key ? [key, params] : null,
-    fetcher,
-    mergeOptions.swr,
-  )
+  const swrResp: SWRResponse = useSWR(key ? [key, params] : null, fetcher, {
+    ...mergeOptions.swr,
+    fallbackData: mergeOptions.initialData,
+  })
 
   // Manual execution request
   const run = (newParams?: any) => {
@@ -62,14 +75,21 @@ const useRequest = (
   // Refresh with the same request parameters (Will not trigger loading)
   const refresh = () => swrResp.mutate({ ...swrResp.data }, true)
 
+  // Update the local data immediately, but disable the revalidation
+  const mutate = (newData: NewData) =>
+    swrResp.mutate({ ...swrResp.data, ...newData }, false)
+
   // Returns whether the loading state is triggered
   const loading = swrResp.data === undefined && swrResp.isValidating
 
   return {
-    ...swrResp,
     loading,
     run,
     refresh,
+    params,
+    ...swrResp,
+    mutate,
+    swrMutate: swrResp.mutate,
   }
 }
 
